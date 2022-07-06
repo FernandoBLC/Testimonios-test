c0e560187eab31f5c6785145be289c71d25e470d
Carlos Fernando Bernal Lopez <nightfuryop@gmail.com>
2022-07-05 22:40:36 -0500

    Testimonios-test v1

---
+++ src/components/Testimonio.js
@@ -0,0 +1,18 @@
+import React from "react"
+import '../componentsCSS/Testimonio.css'
+
+function Testimonio (props) {
+    return (
+        <div className='contenedor-testimonio d-flex align-items-center justify-content-center shadow p-3'>
+            <img className='img-testimonio rounded-1' src={require(`../imgs/${props.imgmichi}`)}
+                 alt={`Foto de ${props.nombremichi}`} />
+            <div className='texto-testimonio text-center p-4 lh-1'>
+                <p className='nombre-monio'><strong>{props.nombremichi}</strong> en {props.residencia}</p>
+                <p className='cargo-monio mt-2  pb-3'>Es Licenciado en {props.carrera} en <strong>{props.empresa}</strong></p>
+                <p className='texto-monio'>"{props.texto}"</p>
+            </div>
+        </div>
+    );
+}
+
+export default Testimonio
